ae02e663793ef77cf31930f0f6c3533fe2a4f620
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2017-09-22 17:02:22 -0400

    Everything we need!

---
+++ src/vue-resource-mocker.js
@@ -0,0 +1,70 @@
+
+import url from 'url';
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+
+class VueResourceMocker {
+
+    constructor() {
+        this.routes = {};
+    }
+
+    install(Vue) {
+        Vue.http.interceptors.length = 0;
+        Vue.http.interceptors.push((request, next) => {
+            let route = this.findRoute(request);
+            if (!route) {
+                next(request.respondWith('File Not Found', {status: 404}));
+            } else {
+                next(route(request));
+            }
+        });
+    }
+
+    setRoutes(routes) {
+        this.routes = routes;
+    }
+
+    findRoute(request) {
+        let byMethod = this.routes[request.method];
+        if (!byMethod) {
+            return null;
+        }
+        let pathname = url.parse(request.getUrl(), true, true).pathname;
+        if (byMethod instanceof Array) {
+            return this.findRouteInArray(pathname, byMethod);
+        } else {
+            return this.findRouteInObject(pathname, byMethod);
+        }
+    }
+
+    findRouteInArray(pathname, array) {
+        let match = array.filter((route) => {
+            if (route.route && route.route.test && route.route.test(pathname)) {
+                return true;
+            } else if (route.route && this.stringToRegex(route.route).test(pathname)) {
+                return true;
+            }
+            return false;
+        })[0];
+        if (match) {
+            return match.use;
+        }
+        return null;
+    }
+
+    findRouteInObject(pathname, object) {
+        let matchKey = Object.keys(object).filter((route) => {
+            return this.stringToRegex(route).test(pathname);
+        })[0];
+        return object[matchKey] || null;
+    }
+
+    stringToRegex(str) {
+        return new RegExp('^' + str.replace(/{[^}]*}/, '.+') + '$');
+    }
+};
+
+module.exports = VueResourceMocker;
---
+++ tests/vue-resource-mocker.spec.js
@@ -0,0 +1,241 @@
+
+import VueResourceMocker from '../src/vue-resource-mocker';
+import assert from 'assert';
+import Vue from 'vue';
+
+describe('VueResourceMocker', function () {
+
+    it('should exist', function () {
+        assert(VueResourceMocker);
+    });
+
+    it('should instantiate', function () {
+        assert(new VueResourceMocker());
+    });
+
+    it('should install', function () {
+        let fakeVue = {
+            http: {
+                interceptors: [],
+            },
+        };
+        new VueResourceMocker().install(fakeVue);
+        assert.equal(1, fakeVue.http.interceptors.length);
+    });
+
+    it('should setRoutes', function () {
+        let routes = {};
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        assert.equal(routes, mocker.routes);
+    });
+
+    it('should instantiate with no routes', function () {
+        let mocker = new VueResourceMocker();
+        assert(mocker.routes, 'Has no routes object');
+        assert.equal(0, Object.keys(mocker.routes).length);
+    });
+
+    it('should findRoute with exact match', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: {
+                '/endpoint': findMe,
+            },
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should not findRoute if method does not exist', function () {
+        let routes = {};
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint'; }
+        });
+        assert.equal(null, route);
+    });
+
+    it('should not findRoute if route does not exist', function () {
+        let routes = {
+            GET: {}
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint'; }
+        });
+        assert.equal(null, route);
+    });
+
+    it('should findRoute with regex match', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: [
+                {
+                    route: /^\/endpoint$/,
+                    use: findMe,
+                }
+            ],
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should not findRoute with regex match', function () {
+        let routes = {
+            GET: [],
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/nothing'; }
+        });
+        assert.equal(null, route);
+    });
+
+    it('should findRoute with array-style exact string match', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: [
+                {
+                    route: '/endpoint',
+                    use: findMe,
+                }
+            ],
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should stringToRegex', function () {
+        let mocker = new VueResourceMocker();
+        let regex;
+
+        regex = mocker.stringToRegex('/endpoint/{id}/go');
+        assert(regex.test('/endpoint/1/go'));
+        assert(!regex.test('X/endpoint/1/goY'));
+
+        regex = mocker.stringToRegex('/endpoint/{id/go');
+        assert(!regex.test('/endpoint/1/go'));
+    });
+
+    it('should findRoute with Laravel-style string match (array)', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: [
+                {
+                    route: '/endpoint/{id}/go',
+                    use: findMe,
+                }
+            ],
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint/1/go'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should findRoute with Laravel-style string match (object)', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: {
+                '/endpoint/{id}/go': findMe,
+            },
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint/1/go'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should findRoute and ignore query params', function () {
+        let findMe = function(){};
+        let routes = {
+            GET: {
+                '/endpoint/{id}/go': findMe,
+            },
+        };
+
+        let mocker = new VueResourceMocker();
+        mocker.setRoutes(routes);
+        let route = mocker.findRoute({
+            method: 'GET',
+            getUrl() { return '/endpoint/1/go?x=1'; }
+        });
+        assert.equal(findMe, route);
+    });
+
+    it('should execute route', function (done) {
+        let capturedRequest = null;
+        let ran = 0;
+        let mocker = new VueResourceMocker();
+        Vue.use(mocker);
+        mocker.setRoutes({
+            GET: {
+                '/endpoint/{id}/go': function (request) {
+                    ran++;
+                    capturedRequest = request;
+                    return request.respondWith({ok: true}, {status: 200});
+                },
+            },
+        });
+
+        Vue.http.get('/endpoint/1/go?super-duper=1')
+            .then(response => {
+                assert(response, 'Response is false: ' + response);
+                assert.equal(1, ran, 'Did not run route');
+                assert.equal('/endpoint/1/go?super-duper=1', capturedRequest.getUrl(), 'Request has odd URL: ' + capturedRequest.getUrl());
+                assert.equal(true, response.data.ok, 'Data is not what was returned.');
+            })
+            .then(done, done);
+    });
+
+    it('should 404', function (done) {
+        let mocker = new VueResourceMocker();
+        Vue.use(mocker);
+        mocker.setRoutes({});
+
+        Vue.http.get('/endpoint/1/go?super-duper=1')
+            .catch(response => {
+                assert(response, 'Response is false: ' + response);
+                assert.equal('File Not Found', response.data, 'Wrong response data: ' + response.data);
+                assert.equal(404, response.status);
+            })
+            .then(done, done);
+    });
+
+});
